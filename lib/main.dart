fdf1664fc5165f4f49451645c29aa6cce3a27fbe
Roshan <rk123517@gmail.com>
2023-05-14 17:14:03 +0530

    d2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_project/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  // const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     // return const Placeholder();
-    return MaterialApp(
-      home: Material(
-          child: Center(
-              child: Container(
-        child: Text("Welcome to 30 Days of Flutter"),
-      ))),
-    );
+    int days = 90;
+    String name = 'Roshan';
+//     double pi = 3.14;
+//     bool isMale = true;
+//     num temp = 30.5;
+
+//     var day = 'Sunday';
+//     const pi = 3.14;   // list can not be modified
+// final //List can be modified
+    return MaterialApp(home: HomePage());
   }
 }
